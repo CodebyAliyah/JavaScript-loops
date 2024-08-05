da20e165d7a66d1e2da92fe8e1f2b4969e441fe9
Ali Yahya <165564206+CodebyAliyah@users.noreply.github.com>
2024-08-04 22:42:24 -0700

    Code Done

---
+++ loops_part2.js
@@ -0,0 +1,34 @@
+// ****** for in loops****** :
+
+const myObject = {
+  US : "United State",
+  PK : "pakistan" ,
+  AFG : "afghanistan" ,
+  CAN : "Canada" ,
+  ENG : "England" ,
+}
+
+for (const key in myObject) {
+  console.log(`${key} shourtcut is for ${myObject[key]}`)
+}
+
+// for in check in aerray : 
+
+const myArray = ["sir hitesh" , "sir Naveed sarwar" , "sir Umair" , "sir saad" ]
+
+for (const key in myArray) {
+  console.log (myArray[key])
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
